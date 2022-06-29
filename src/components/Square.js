4f46b8e9d661c774bc74a99a9608bccf5b2072a7
Rejnszu <lukaszrejnsz@gmail.com>
2022-06-29 14:42:38 +0200

    quite smarter computer

--- src/components/Square.js
+++ src/components/Square.js
@@ -14,6 +14,7 @@ let scoreBoard = [
   ["", false],
   ["", false],
 ];
+
 let randomNumber;
 export default function Square(props) {
   const context = useContext(TicContext);
@@ -52,13 +53,180 @@ export default function Square(props) {
   }
 
   function AutoPlay() {
-    do {
-      randomNumber = Math.round(Math.random() * (scoreBoard.length - 1));
-    } while (scoreBoard[randomNumber][0] !== "");
+    // BLOCK CONDITIONS
+    if (
+      scoreBoard[0][0] === "X" &&
+      scoreBoard[1][0] === "X" &&
+      scoreBoard[2][0] === ""
+    ) {
+      scoreBoard[2][0] = "O";
+      scoreBoard[2][1] = true;
+    } else if (
+      scoreBoard[0][0] === "X" &&
+      scoreBoard[2][0] === "X" &&
+      scoreBoard[1][0] === ""
+    ) {
+      scoreBoard[1][0] = "O";
+      scoreBoard[1][1] = true;
+    } else if (
+      scoreBoard[1][0] === "X" &&
+      scoreBoard[2][0] === "X" &&
+      scoreBoard[0][0] === ""
+    ) {
+      scoreBoard[0][0] = "O";
+      scoreBoard[0][1] = true;
+    } else if (
+      scoreBoard[3][0] === "X" &&
+      scoreBoard[4][0] === "X" &&
+      scoreBoard[5][0] === ""
+    ) {
+      scoreBoard[5][0] = "O";
+      scoreBoard[5][1] = true;
+    } else if (
+      scoreBoard[3][0] === "X" &&
+      scoreBoard[5][0] === "X" &&
+      scoreBoard[4][0] === ""
+    ) {
+      scoreBoard[4][0] = "O";
+      scoreBoard[4][1] = true;
+    } else if (
+      scoreBoard[4][0] === "X" &&
+      scoreBoard[5][0] === "X" &&
+      scoreBoard[3][0] === ""
+    ) {
+      scoreBoard[3][0] = "O";
+      scoreBoard[3][1] = true;
+    } else if (
+      scoreBoard[6][0] === "X" &&
+      scoreBoard[7][0] === "X" &&
+      scoreBoard[8][0] === ""
+    ) {
+      scoreBoard[8][0] = "O";
+      scoreBoard[8][1] = true;
+    } else if (
+      scoreBoard[6][0] === "X" &&
+      scoreBoard[8][0] === "X" &&
+      scoreBoard[7][0] === ""
+    ) {
+      scoreBoard[7][0] = "O";
+      scoreBoard[7][1] = true;
+    } else if (
+      scoreBoard[7][0] === "X" &&
+      scoreBoard[8][0] === "X" &&
+      scoreBoard[6][0] === ""
+    ) {
+      scoreBoard[6][0] = "O";
+      scoreBoard[6][1] = true;
+    } else if (
+      scoreBoard[0][0] === "X" &&
+      scoreBoard[3][0] === "X" &&
+      scoreBoard[6][0] === ""
+    ) {
+      scoreBoard[6][0] = "O";
+      scoreBoard[6][1] = true;
+    } else if (
+      scoreBoard[0][0] === "X" &&
+      scoreBoard[6][0] === "X" &&
+      scoreBoard[3][0] === ""
+    ) {
+      scoreBoard[3][0] = "O";
+      scoreBoard[3][1] = true;
+    } else if (
+      scoreBoard[3][0] === "X" &&
+      scoreBoard[6][0] === "X" &&
+      scoreBoard[0][0] === ""
+    ) {
+      scoreBoard[0][0] = "O";
+      scoreBoard[0][1] = true;
+    } else if (
+      scoreBoard[1][0] === "X" &&
+      scoreBoard[4][0] === "X" &&
+      scoreBoard[7][0] === ""
+    ) {
+      scoreBoard[7][0] = "O";
+      scoreBoard[7][1] = true;
+    } else if (
+      scoreBoard[1][0] === "X" &&
+      scoreBoard[7][0] === "X" &&
+      scoreBoard[4][0] === ""
+    ) {
+      scoreBoard[4][0] = "O";
+      scoreBoard[4][1] = true;
+    } else if (
+      scoreBoard[4][0] === "X" &&
+      scoreBoard[7][0] === "X" &&
+      scoreBoard[1][0] === ""
+    ) {
+      scoreBoard[1][0] = "O";
+      scoreBoard[1][1] = true;
+    } else if (
+      scoreBoard[2][0] === "X" &&
+      scoreBoard[5][0] === "X" &&
+      scoreBoard[8][0] === ""
+    ) {
+      scoreBoard[8][0] = "O";
+      scoreBoard[8][1] = true;
+    } else if (
+      scoreBoard[2][0] === "X" &&
+      scoreBoard[8][0] === "X" &&
+      scoreBoard[5][0] === ""
+    ) {
+      scoreBoard[5][0] = "O";
+      scoreBoard[5][1] = true;
+    } else if (
+      scoreBoard[5][0] === "X" &&
+      scoreBoard[8][0] === "X" &&
+      scoreBoard[2][0] === ""
+    ) {
+      scoreBoard[2][0] = "O";
+      scoreBoard[2][1] = true;
+    } else if (
+      scoreBoard[0][0] === "X" &&
+      scoreBoard[4][0] === "X" &&
+      scoreBoard[8][0] === ""
+    ) {
+      scoreBoard[8][0] = "O";
+      scoreBoard[8][1] = true;
+    } else if (
+      scoreBoard[0][0] === "X" &&
+      scoreBoard[8][0] === "X" &&
+      scoreBoard[4][0] === ""
+    ) {
+      scoreBoard[4][0] = "O";
+      scoreBoard[4][1] = true;
+    } else if (
+      scoreBoard[4][0] === "X" &&
+      scoreBoard[8][0] === "X" &&
+      scoreBoard[0][0] === ""
+    ) {
+      scoreBoard[0][0] = "O";
+      scoreBoard[0][1] = true;
+    }
+    // Basic conditions
+    else if (scoreBoard[4][0] === "") {
+      scoreBoard[4][0] = "O";
+      scoreBoard[4][1] = true;
+      console.log("wtf");
+    } else if (scoreBoard[6][0] === "") {
+      scoreBoard[6][0] = "O";
+      scoreBoard[6][1] = true;
+    } else if (scoreBoard[2][0] === "") {
+      scoreBoard[2][0] = "O";
+      scoreBoard[2][1] = true;
+    } else if (scoreBoard[8][0] === "") {
+      scoreBoard[8][0] = "O";
+      scoreBoard[8][1] = true;
+    } else if (scoreBoard[0][0] === "") {
+      scoreBoard[0][0] = "O";
+      scoreBoard[0][1] = true;
+    } else {
+      do {
+        randomNumber = Math.round(Math.random() * (scoreBoard.length - 1));
+      } while (scoreBoard[randomNumber][0] !== "");
 
-    scoreBoard[randomNumber][0] = "O";
-    scoreBoard[randomNumber][1] = true;
-
+      scoreBoard[randomNumber][0] = "O";
+      scoreBoard[randomNumber][1] = true;
+    }
     props.checkWinner();
     computerTurn = false;
   }
